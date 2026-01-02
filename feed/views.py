44713af0f720501a0d4dbfce84760d296fbab8f7
Mamane Bachir <mamanebachirbalarabe72@gmail.com>
2026-01-02 16:31:54 +0100

    mise à jour

--- feed/views.py
+++ feed/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from feed.models import Message
 # Create your views here.
 def index(request):
-   contexte = {}   
-   
+   if request.method == 'POST':
+      content = request.POST.get('content')
+      user = request.user
+      print(content, user)
+      Message.objects.create(content = content, user = user)
+   contexte = {}
    contexte ['messages'] = Message.objects.order_by('-created_at')
 
    return render(request, template_name= 'index.html', context=contexte)
